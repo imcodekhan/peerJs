88a64400893f4301280cc930e94ee6c840367051
imcodekhan <imkhan.mdamir@outlook.com>
2023-10-28 02:50:42 +0530

    wip

--- src/Pages/Onboarding/index.jsx
+++ src/Pages/Onboarding/index.jsx
@@ -1,34 +1,53 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import Layout from "../../Components/Layout";
 import Introduction from "./Introduction";
 import Registration from "./Registration";
 import About from "./About";
-import { STEPS } from "../../constants";
-import { registerUser } from "../../Services/crud";
+import { ROUTES, STEPS } from "../../constants";
+import { registerUser, updatedUser } from "../../Services/crud";
+import useUserContext from "../../Context/UserProvider/useUserContext";
+import {
+  updateName,
+  updatePhoneNumber,
+} from "../../Context/UserProvider/userActions";
 
 const Onboarding = () => {
-  const [step, setstep] = useState(STEPS.INTRODUCTION);
-
+  const [step, setStep] = useState(STEPS.INTRODUCTION);
+  const { state, dispatch } = useUserContext();
   const navigate = useNavigate();
 
+  // Check if the phone number is already in local storage and skip onboarding if it is.
   useEffect(() => {
     const localPhoneNumber = localStorage.getItem("phoneNumber");
     if (localPhoneNumber) {
-      navigate("/homepage");
+      navigate(ROUTES.HOMEPAGE);
     }
-  }, []);
+  }, [navigate]);
 
-  function handleRegistration(phoneNumber) {
-    const { success } = registerUser(phoneNumber);
-    console.log({ success });
-  }
-  function handleAboutUpdate() {}
+  const handleRegistration = async (phoneNumber) => {
+    const { success } = await registerUser(phoneNumber);
+    if (success) {
+      updateUserDataAndNavigate(phoneNumber, STEPS.ABOUT);
+    }
+  };
+
+  const handleAboutUpdate = async (name) => {
+    const { success } = await updatedUser(state.phoneNumber, name);
+    if (success) {
+      updateUserDataAndNavigate(state.phoneNumber, ROUTES.HOMEPAGE);
+    }
+  };
+
+  const updateUserDataAndNavigate = (phoneNumber, nextStep) => {
+    localStorage.setItem("phoneNumber", phoneNumber);
+    dispatch(updatePhoneNumber(phoneNumber));
+    setStep(nextStep);
+  };
 
   const renderStep = {
     [STEPS.INTRODUCTION]: (
-      <Introduction handleNextStep={({ step }) => setstep(step)} />
+      <Introduction handleNextStep={({ step }) => setStep(step)} />
     ),
     [STEPS.REGISTRATION]: (
       <Registration handleRegistration={handleRegistration} />
